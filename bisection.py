da2dc5928c17ec3da19ffad88ba12d7e94871aed
Sergei Shumilin <ishumili@gmail.com>
2019-03-20 13:21:42 +0300

    bisection for finding elements in an array for interpolation

---
+++ bisection.py
@@ -0,0 +1,30 @@
+"""Задача найти элемент в массиве >= заданному"""
+import random
+import math
+
+if __name__ == '__main__':
+    # Массив для поиска элемента.
+    array = range(0, 50)
+    array = sorted(array)
+    x = 40.84
+    a = 0
+    b = len(array) - 1
+    m = (b + a) // 2
+    print(x)
+    print(array)
+
+    while b - a > 1:
+
+        if x == array[m]:
+            a = m - 1
+            b = m
+            break
+
+        if x < array[m]:
+            b = m
+            m = (b + a) // 2
+        else:
+            a = m
+            m = (b + a) // 2
+
+    print('Ответ: ' + str(a) + ' > x > ' + str(b))
